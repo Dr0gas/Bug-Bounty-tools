53e8b0b357340ede1f047ed8e0f491f130a0c3e3
Dr0gas <douglas_cpt@hotmail.com>
2023-01-09 12:35:09 -0300

    Update bb_tools.sh

--- bb_tools.sh
+++ bb_tools.sh
@@ -220,6 +220,9 @@ go install github.com/tomnomnom/meg@latest
 git -C ~/tools clone https://github.com/Viralmaniar/MurMurHash.git
 pip3 install -r ~/tools/MurMurHash/requirements.txt
 
+# Mx Takeover
+go install -v github.com/musana/mx-takeover@latest
+
 # Ngrok
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 ngrok update 
